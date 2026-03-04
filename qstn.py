a6b23c5fc95b7cccf15e49d3ad448e33d43dfec9
rishana <rishhana5@gmail.com>
2026-03-04 12:21:37 -0800

    answers

---
+++ qstn.py
@@ -0,0 +1,86 @@
+# tuple=(1,2,3,4)
+# print(tuple.index(3))
+
+# tpl=(10,20,30)
+# new_tpl= list(tpl)
+# tpl=list(new_tpl)
+# print(type(tpl))
+
+# lst=[1,2,3]
+# new_lst=tuple(lst)
+# lst=tuple(new_lst)
+# print(type(lst))
+
+# slc=("a","b","c","d")
+# print(slc[1:3])
+
+# x1=("x","y","z")
+# if "x" in x1:
+#     print("yes")
+# else:
+#     print("no")
+
+# mx=(5,3,9,1)
+# print(max(mx))
+
+# a=(1,2,3)
+# a=a+a
+# print(a)
+
+# tp=(1,2,2,3,2)
+# print(tp.count(2))
+
+# ind=("dog","cat","mouse")
+# print(ind.index("cat"))
+
+# rvs=(1,2,3,4,5)
+# print(rvs[::-1])
+
+# f=(1,2)
+# g=(3,4)
+# print(f+g)
+
+# cvrt=("hello")  
+# stng=tuple(cvrt)  
+# cvrt=tuple(stng)                       
+# print(cvrt)
+
+# a=(1,2,3,4)
+# b=list(a)
+# a=list(b)           #pendinggggggggg
+
+# print(a)
+
+
+# rplc=(10,20,30,40)
+# lst=list(rplc)
+# rplc=list(lst)
+# rplc[2:3]=[99]
+# print(rplc)
+
+# exct=(1,2,3)
+# (a,b,c)=(exct)
+
+# print(a)
+# print(b)
+# print(c)
+
+#//////////////
+
+
+# a=("a","b")
+# b=["c","d"]
+
+# print(a+ tuple(b))
+
+# check=(1,2,3)
+# if check==check[::-1]:
+#     print("equal")
+# else:
+#     print("not equal")
+
+
+
+
+
+
