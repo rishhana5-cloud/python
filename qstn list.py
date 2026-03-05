85a387ff1df3401afcce7aad837849c6ab1d0be9
rishana <rishhana5@gmail.com>
2026-03-04 21:11:24 -0800

    list answers

---
+++ qstn list.py
@@ -0,0 +1,81 @@
+
+# a=[1,2,3]
+# a.append(4)
+# print(a)
+
+# b= [10,20,30]
+# b.remove(20)
+# print(b)
+
+# asnd=[5,3,9,1] 
+# asnd.sort()
+# print(asnd)
+
+# slc=[1,2,3,4,5]
+# print(slc[1:4]) 
+
+# rvrs=[1,2,3,4]
+# print(rvrs[::-1])
+
+# a=[1,2]
+# b=[3,4]
+# c=a+b
+# print(c)
+
+# cvrt=[7,8]
+# a=cvrt+cvrt
+# print(a)
+
+# chk=[1,2,3,4]
+# if   3 in chk:
+#     print("yes")
+# else:
+#     print("no")
+
+
+# cnt=[1,2,2,3,2]
+# print(cnt.count(2))
+
+# rmv=["a","b","c","d"]
+# rmv.pop()
+# print(rmv)
+
+# ad= ["a","b","c"]
+# ad.insert(1,"x")
+# print(ad)
+
+# nmbr=[10,20,30,40]
+# nmbr[2]=99
+# print(nmbr)
+
+# rng=list(range(5))
+# print(rng)
+
+# splc=[1,2,3,4,5,6]
+# print(splc[1::2])
+
+# rmv= [1,2,3]
+# rmv.clear()
+# print(rmv)
+
+# cpy=[4,5,6]
+# cpy.copy()
+# print(cpy)
+
+
+# nest=[1,2,3]
+# nest=[nest]
+# print(nest)
+
+# lst=[1,2]
+# lst1=[3,4,5]
+# lst.extend(lst1)
+# print(lst)
+
+# rpt=["hello"]
+# rpt=rpt*3
+# print(rpt)
+
+# remv=[10,20,30,40]
+# remv.pop(2)
+# print(remv)
