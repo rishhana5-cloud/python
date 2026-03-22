0ed82d8aa52924a36f91c6b571eba2c54725bf9b
rishana <rishhana5@gmail.com>
2026-03-22 08:19:53 -0700

    function.py

---
+++ function.py
@@ -0,0 +1,36 @@
+
+# def hello ():
+#     print("how are you")
+
+# hello()
+
+
+# def hi (name, age):
+#  print(f'my name is {name} my age is {age}')
+
+# hi("rishana",20)
+# hi("divya",5)
+
+
+         #types of arguments
+
+# def hello (name, age):
+#  print(f'my name is {name} my age is {age}')       #positional argument
+
+# hi("jish",25)
+
+
+# def key (name,age):
+#  print(f'my name is {name} my age is {age}')        #keyword argument
+# key(age=25,name="ani")
+
+
+# def  fault(name="guest"):
+#     print(f'my name is {name}')            #default argument
+
+# fault()
+# fault("vish")
+
+
+
+
